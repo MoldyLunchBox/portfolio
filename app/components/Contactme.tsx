931e799bada11db5c5e0ce1b0c00f298c50793f6
MoldyLunchBox <duu116@gmail.com>
2023-07-01 17:42:48 +0100

    experementing with 3d objects

--- app/components/Contactme.tsx
+++ app/components/Contactme.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import {Map} from "./Map"
 import emailjs from '@emailjs/browser';
 
@@ -7,24 +7,36 @@ import emailjs from '@emailjs/browser';
 // use client
 export const Contactme = () => {
   const form = useRef<HTMLFormElement>(null);
+  const [success, setSuccess] = useState(false)
   const handleSubmit = (e: any) =>{
     e.preventDefault()
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-    .then((result) => {
-        console.log(result.text);
-    }, (error) => {
-        console.log(error.text);
-    });
+    if (form.current){
+      try{
+
+        emailjs.sendForm('service_dfbzv2d', 'template_gsuwpmr', form.current, 'LTyAnzVB5cBw9Swnl')
+        .then((result) => {
+          console.log(result.text);
+          setSuccess(true)
+        }, (error) => {
+          console.log(error.text);
+          setSuccess(false)
+        });
+      }
+      catch (error){
+        setSuccess(false)
+      }
+    }
   }
   return (
     <div className='flex items-center justify-between snap-center h-[100vh]'>
 <div className="basis-1/2 flex justify-end   ">
       <form action="" ref={form} className="flex  w-full max-w-[450px] flex-col gap-4">
         <h1 className="text-white"> Contact me</h1>
-        <input placeholder="Title" className="bg-[#e8e8e6] p-2 rounded-[2px]" type="text" />
-        <input placeholder="Your email" className="bg-[#e8e8e6] p-2 rounded-[2px]" type="text" />
-        <textarea placeholder="Type your message" className="bg-[#e8e8e6] rounded-[2px] p-4" name="" id="" cols="30" rows="10"></textarea>
+        <input placeholder="Title" name="title" className="bg-[#e8e8e6] p-2 rounded-[2px]" type="text" />
+        <input placeholder="Your email" name="email" className="bg-[#e8e8e6] p-2 rounded-[2px]" type="email" />
+        <textarea placeholder="Type your message" name="message" className="bg-[#e8e8e6] rounded-[2px] p-4" cols="30" rows="10"></textarea>
         <button onClick={handleSubmit} className="bg-[yellow]"> Send</button>
+        {success ? "Message sent succesfully" : null}
       </form>
 </div>
       <div className=" h-full text-white basis-1/2">
--- app/components/Whoami.tsx
+++ app/components/Whoami.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useRef, useState } from 'react';
 import { Navbar } from './Navbar';
 import Image from 'next/image';
+import {Canvas} from '@react-three/fiber'
+import {OrbitControls} from '@react-three/drei'
+import { PerspectiveCamera } from 'three';
+
+import {Mesh, Shape,DoubleSide , ShapeGeometry, MeshBasicMaterial, Scene, BoxGeometry } from 'three';
 
 export const Whoami = () => {
+  const [heart , setHeart] = useState({shape:new ShapeGeometry(), material: new MeshBasicMaterial()})
 
+
+useEffect(()=>{
+  const x = 0, y = 0;
+
+const heartShape = new Shape();
+
+heartShape.moveTo(x + 5, y + 5);
+heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
+heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
+heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
+heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
+heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
+heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
+
+
+const geometry = new ShapeGeometry( heartShape );
+const material = new MeshBasicMaterial( { color: 0x00ff00,side: DoubleSide  } );
+// Center the mesh
+geometry.center();
+const mesh = new Mesh( geometry, material ) ;
+const camera = new PerspectiveCamera(100, 5, 0.1, 1000);
+mesh.scale.set(0.5, 0.5, 0.5); // Adjust the scale to make it smaller
+
+const scene = new Scene();
+scene.add( mesh );
+scene.add(camera);
+setHeart({shape: geometry, material: material})
+})
   return (
     <div className='flex items-center snap-center h-[100vh]'>
       <div className='flex justify-between items-center h-full w-full'>
         <div className='w-full flex justify-center'>
-     
+     <Canvas>
+        <OrbitControls autoRotate />
+      <mesh scale={[0.4, -0.4, 0.4]} >
+        {/* <shapeGeometry args={[heart]}/> */}
+        <mesh args={[heart.shape, heart.material]}/>
+        {/* <shapeGeometry args={[new Shape()]} /> */}
+      </mesh>
+     </Canvas>
         </div>
         <div className='flex w-full flex-col justify-start gap-4'>
           <h1 className='text-white text-4xl font-bold'>Feel, Think, Design</h1>
