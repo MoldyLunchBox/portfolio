123236343271b5bf4ecd4db9cfbe6c2414f9df98
MoldyLunchBox <duu116@gmail.com>
2023-07-02 06:13:26 +0100

    ..

---
+++ app/components/Heart.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+
+import { Mesh, Shape, DoubleSide, ShapeGeometry, MeshBasicMaterial, Scene, BoxGeometry, WebGLRenderer } from 'three';
+
+export const Heart = () => {
+    const angleToradians = (degAngle: number) => (Math.PI / 180) * degAngle
+    const orbitControlsRef = useRef(null)
+    useFrame((state) => {
+        if (orbitControlsRef.current) {
+            const { x, y } = state.mouse
+            orbitControlsRef.current.setAzimuthalAngle(-x * angleToradians(90))
+            orbitControlsRef.current.setPolarAngle(-y+0.9 * angleToradians(90-30))
+
+            orbitControlsRef.current.update()
+        }
+    })
+    useEffect(() => {
+        if (orbitControlsRef.current)
+            console.log(orbitControlsRef.current)
+    }, [orbitControlsRef.current])
+    return (
+        <>
+            {/* <mesh scale={[0.4, -0.4, 0.4]} >
+    <ambientLight intensity={1} />
+    <directionalLight position={[3,2,1]} />
+    <mesh args={[heart.shape, heart.material]}/>
+    <meshStandardMaterial  color={"red"}/>
+</mesh> */}
+            {/* <shapeGeometry args={[heart]}/> */}
+            {/* <shapeGeometry args={[new Shape()]} /> */}
+
+
+            <PerspectiveCamera  makeDefault position={[0, 3, 15]} />
+            <OrbitControls  ref={orbitControlsRef} maxPolarAngle={ angleToradians(80)} minPolarAngle={angleToradians(60)} />
+            <mesh position={[0, 1, 0]} castShadow >
+                <sphereGeometry args={[1, 32, 32]} />
+                <meshStandardMaterial color="yellow" />
+            </mesh>
+            <mesh rotation={[-angleToradians(90), 0, 0]} receiveShadow>
+                <planeGeometry args={[7, 7]} />
+                <meshStandardMaterial color="white" />
+
+            </mesh>
+            <ambientLight intensity={0.01} />
+            {/* directinal light */}
+            {/* <directionalLight args={["white", 1]} position={[-40,20,10]} /> */}
+            <spotLight  penumbra={0.3} args={["white", 2, 20, 0.3]} castShadow position={[-5,5,0]}/>
+        </>
+    )
+}
--- app/components/Whoami.tsx
+++ app/components/Whoami.tsx
@@ -2,68 +2,60 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Navbar } from './Navbar';
 import Image from 'next/image';
-import {Canvas} from '@react-three/fiber'
-import {OrbitControls} from '@react-three/drei'
-import { PerspectiveCamera } from 'three';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 
-import {Mesh, Shape,DoubleSide , ShapeGeometry, MeshBasicMaterial, Scene, BoxGeometry , WebGLRenderer } from 'three';
+import { Mesh, Shape, DoubleSide, ShapeGeometry, MeshBasicMaterial, Scene, BoxGeometry, WebGLRenderer } from 'three';
+import { Heart } from './Heart';
 
 export const Whoami = () => {
-  const [heart , setHeart] = useState({shape:new ShapeGeometry(), material: new MeshBasicMaterial()})
+  const [heart, setHeart] = useState({ shape: new ShapeGeometry(), material: new MeshBasicMaterial() })
 
 
-useEffect(()=>{
-  const x = 0, y = 0;
+  // useEffect(()=>{
+  //   const x = 0, y = 0;
 
-const heartShape = new Shape();
+  // const heartShape = new Shape();
 
-heartShape.moveTo(x + 5, y + 5);
-heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
-heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
-heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
-heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
-heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
-heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
-
-
-const geometry = new ShapeGeometry( heartShape );
-const material = new MeshBasicMaterial( { color: 0x00ff00,side: DoubleSide  } );
-// Center the mesh
-const scene = new Scene();
-geometry.center();
-const mesh = new Mesh( geometry, material ) ;
-mesh.scale.set(0.5, 0.5, 0.5); // Adjust the scale to make it smaller
+  // heartShape.moveTo(x + 5, y + 5);
+  // heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
+  // heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
+  // heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
+  // heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
+  // heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
+  // heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
 
 
-    // Adjust camera position and field of view
-    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 10;
-    
-    scene.add(mesh);
-    scene.add(camera);
+  // const geometry = new ShapeGeometry( heartShape );
+  // const material = new MeshBasicMaterial( { color: 0x00ff00,side: DoubleSide  } );
+  // // Center the mesh
+  // const scene = new Scene();
+  // geometry.center();
+  // const mesh = new Mesh( geometry, material ) ;
+  // mesh.scale.set(0.5, 0.5, 0.5); // Adjust the scale to make it smaller
 
 
+  //     // Adjust camera position and field of view
+  //     const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  //     camera.position.z = 10;
+
+  //     scene.add(mesh);
+  //     scene.add(camera);
 
 
 
-scene.add( mesh );
-setHeart({shape: geometry, material: material})
-})
+
+
+  // scene.add( mesh );
+  // setHeart({shape: geometry, material: material})
+  // })
   return (
     <div className='flex items-center snap-center h-[100vh]'>
-      <div className='flex justify-between items-center h-full w-full'>
-        <div className='w-full flex justify-center'>
-     <Canvas>
-        <OrbitControls autoRotate />
-      <mesh scale={[0.4, -0.4, 0.4]} >
-        <ambientLight intensity={1} />
-        <directionalLight position={[3,2,1]} />
-        {/* <shapeGeometry args={[heart]}/> */}
-        <mesh args={[heart.shape, heart.material]}/>
-        <meshStandardMaterial  color={"red"}/>
-        {/* <shapeGeometry args={[new Shape()]} /> */}
-      </mesh>
-     </Canvas>
+      <div className='flex justify-between flex-col   items-center h-full w-full lg:flex-row'>
+        <div className='w-full h-full max-h-[50vh]'>
+          <Canvas shadows>
+            <Heart />
+          </Canvas>
         </div>
         <div className='flex w-full flex-col justify-start gap-4'>
           <h1 className='text-white text-4xl font-bold'>Feel, Think, Design</h1>
